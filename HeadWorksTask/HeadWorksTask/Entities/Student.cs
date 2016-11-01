8d5338d6f2136378302378ed363a868c063ed355
FedorVoloshyn <tikey.gm@gmail.com>
2016-11-02 00:49:00 +0200

    StudentListPage and binding to VM

--- HeadWorksTask/HeadWorksTask/Entities/Student.cs
+++ HeadWorksTask/HeadWorksTask/Entities/Student.cs
@@ -2,15 +2,20 @@
 {
     public struct Student
     {
-        string Name { get; set; }
-        string SecondName { get; set; }
-        string Group { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Group { get; set; }
 
         public Student(string _name, string _secondName, string _group)
         {
             this.Name = _name;
             this.SecondName = _secondName;
             this.Group = _group;
+        }
+
+        public string FullStudentName
+        {
+            get { return this.Name + ' ' + this.SecondName + ". Group #" + this.Group; }
         }
     }
 }
